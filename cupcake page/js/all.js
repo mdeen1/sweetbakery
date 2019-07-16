1823d3781abfa07cf8caef5f01a08e85e9d06712
mdeen1 <34013848+mdeen1@users.noreply.github.com>
2019-07-16 08:29:57 -0400

    made shopping cart showlist

---
+++ cupcake page/js/all.js
@@ -0,0 +1,26 @@
+/*!
+ * Font Awesome Free 5.2.0 by @fontawesome - https://fontawesome.com
+ * License - https://fontawesome.com/license (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License)
+ */
+(function () {
+'use strict';
+
+var _WINDOW = {};
+try {
+  if (typeof window !== 'undefined') _WINDOW = window;
+  
+} catch (e) {}
+
+var _ref = _WINDOW.navigator || {};
+var _ref$userAgent = _ref.userAgent;
+var userAgent = _ref$userAgent === undefined ? '' : _ref$userAgent;
+
+var WINDOW = _WINDOW;
+
+
+
+
+
+var IS_IE = ~userAgent.indexOf('MSIE') || ~userAgent.indexOf('Trident/');
+
+var NAMESPACE_IDENTIFIER = '___FONT_AWESOME___';
---
+++ cupcake page/js/app.js
@@ -0,0 +1,43 @@
+// show/hide cart
+
+document.getElementById('cart-info').addEventListener('click', () => {
+  document.getElementById('cart').classList.toggle('show-cart');
+})
+
+
+// show cart
+
+(function() {
+	const cartInfo = document.getElementById("cart-info");
+	const cart = document.getElementById("cart");
+
+	cartInfo.addEventListener("click", function() {
+		cart.classList.toggle("show-cart");
+	});
+})();
+
+
+// add items to the cart 
+
+(function() {
+	const cartBtn = document.querySelectorAll(".store-item-icon");
+
+	cartBtn.forEach(function(btn) {
+		btn.addEventListener("click", function(event) {
+			// console.log(event.target);
+
+			if (event.target.parentElement.classList.contains("store-item-icon"))
+			{
+				let fullPath =
+				event.target.parentElement.previousElementSibling.src;
+				let pos = fullPath.indexOf("img") + 3;
+				let partPath = fullPath.slice(pos);
+
+				const item = {};
+				item.img = 'img-cart${partPath}';
+
+				console.log(item);
+			}
+		});
+	});
+})();
